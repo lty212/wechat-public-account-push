87fa6a2c7cff08ee61628300edf326b5e99e92b9
lty212 <114340451+lty212@users.noreply.github.com>
2022-09-25 19:57:22 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -69,7 +69,7 @@ const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: '',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: '我的宝宝',
       // 所在省份
       province: '广东',
       // 所在城市
